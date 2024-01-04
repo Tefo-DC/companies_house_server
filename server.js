a4aea19fcbb7b46402477ef2b151012ff82f0e0b
Tefo-DC <tefo.keabetswe@digitalclouduk.com>
2024-01-04 11:19:15 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,38 @@
+// server.js
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/search", async (req, res) => {
+  const apiKey = "e2902688-21fe-4f46-ab47-d98a8d4f73f4";
+  const { query } = req.body;
+
+  try {
+    const response = await axios.get(
+      `https://api.companieshouse.gov.uk/search/companies?q=${query}`,
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(apiKey).toString("base64")}`,
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(
+      "Error fetching data from Companies House API:",
+      error.message
+    );
+    console.error("Request headers:", error.config.headers);
+    res.status(error.response?.status || 500).json({ error: error.message });
+  }
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
